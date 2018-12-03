3114b04b81a06f65323901c24144aa09ad5fed2b
Damodar Lohani <dlohani48@gmail.com>
2018-12-03 12:37:31 +0545

    navigating to new screen

--- lib/main.dart
+++ lib/main.dart
@@ -55,11 +55,46 @@ class RandomWordsState extends State<RandomWords> {
     );
   }
 
+  void _pushSaved() {
+  Navigator.of(context).push(
+    new MaterialPageRoute<void>(
+      builder: (BuildContext context) {
+        final Iterable<ListTile> tiles = _saved.map(
+          (WordPair pair) {
+            return new ListTile(
+              title: new Text(
+                pair.asPascalCase,
+                style: _biggerFont,
+              ),
+            );
+          },
+        );
+        final List<Widget> divided = ListTile
+          .divideTiles(
+            context: context,
+            tiles: tiles,
+          )
+          .toList();
+        
+        return new Scaffold(
+          appBar: AppBar(
+            title: const Text('Saved Suggestions')
+          ),
+          body: new ListView(children: divided)
+        );
+      },
+    ),
+  );
+}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Startup Name Generator'),
+        actions: <Widget>[
+          new IconButton(icon: const Icon(Icons.list), onPressed: _pushSaved,)
+        ],
       ),
       body: _buildSuggestions(),
     );
